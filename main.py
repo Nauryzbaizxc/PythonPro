9acc34df27cf80be13b5a312fd872338a6e2ef03
Nauryzbaizxc <nauryzbajakbota@gmail.com>
2024-06-15 13:58:01 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,35 @@
+from Ghoal import settings
+import discord
+from bot_logic import flip_coin
+from bot_logic import gen_emodji
+from bot_logic import gen_pass
+# Переменная intents - хранит привилегии бота
+intents = discord.Intents.default()
+# Включаем привелегию на чтение сообщений
+intents.message_content = True
+# Создаем бота в переменной client и передаем все привелегии
+client = discord.Client(intents=discord.Intents.default())
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content.startswith('$hello'):
+        await message.channel.send('Привет! Я бот!')
+    elif message.content.startswith('$smile'):
+        await message.channel.send(gen_emodji())
+    elif message.content.startswith('$coin'):
+        await message.channel.send(flip_coin())
+    elif message.content.startswith('$pass'):
+        await message.channel.send(gen_pass(10))
+    elif message.content.startswith('$bye'):
+        await message.channel.send("\\U0001f642")
+    else:
+        await message.channel.send(message.content)
+
+client.run(TOKEN)
+
 
